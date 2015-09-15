44eb01172b3f2997d0145ec8faaaa8ab18b9bb77
Clayton Radke <cradke3@studentmail.nait.ca>
2015-09-15 15:13:00 -0600

    InClassDemos
    More testing of GitHub

---
+++ Solution1/Solution1WebSite/App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Solution1WebSite.Startup))]
+namespace Solution1WebSite
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
